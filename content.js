9af0c0e12805686e30cb05cd3b69152487ec56d8
Darkempire78 <clement.wawszczyk@gmail.com>
2021-02-11 15:54:51 +0100

    add options

--- content.js
+++ content.js
@@ -1,10 +1,33 @@
-// Create the Github1s button
-var btn = document.createElement("a");
-btn.innerHTML = "View on Github1s";
-btn.classList = "btn ml-2 d-none d-md-block";
-btn.href = window.location.href.replace("https://github.com/", "https://github1s.com/");
 buttonNav = document.getElementsByClassName("file-navigation mb-3 d-flex flex-items-start");
 
 if (buttonNav[0] != null) {
-    buttonNav[0].appendChild(btn);
+    // Options
+    chrome.storage.sync.get(["backgroundColor", "textColor", "openInNewPage"], function (obj) {  
+        var backgroundColor = obj.backgroundColor;
+        var textColor = obj.textColor;
+        var openInNewPage = obj.openInNewPage;
+
+        // Create the Github1s button
+        var btn = document.createElement("a");
+        btn.innerHTML = "View on Github1s";
+        btn.classList = "btn ml-2 d-none d-md-block";
+        btn.href = window.location.href.replace("https://github.com/", "https://github1s.com/");
+
+        if (openInNewPage) {
+            btn.target = "_blank"
+        }
+
+        if (!backgroundColor.startsWith("#")){
+            backgroundColor = `#${backgroundColor}`
+        }
+        if (!textColor.startsWith("#")){
+            textColor = `#${textColor}`
+        }
+
+        btn.style.backgroundColor = backgroundColor;
+        btn.style.color = textColor;
+        
+        // Add the button
+        buttonNav[0].appendChild(btn);
+    });
 }
---
+++ options.js
@@ -0,0 +1,37 @@
+// https://developer.chrome.com/docs/extensions/mv2/options/
+
+// Saves options to chrome.storage
+function save_options() {
+    var backgroundColor = document.getElementById('backgroundColor').value;
+    var textColor = document.getElementById('textColor').value;
+    var openInNewPage = document.getElementById('openInNewPage').checked;
+    chrome.storage.sync.set({
+        textColor: textColor,
+        backgroundColor: backgroundColor,
+        openInNewPage: openInNewPage
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+  
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value
+    chrome.storage.sync.get({
+        backgroundColor: '#fafbfc',
+        textColor: "#24292e",
+        openInNewPage: false
+    }, function(items) {
+        document.getElementById('backgroundColor').value = items.backgroundColor;
+        document.getElementById('textColor').value = items.textColor;
+        document.getElementById('openInNewPage').checked = items.openInNewPage;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
