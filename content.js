a27f4c260590a49be7910931d4a238ee3ac8a474
Darkempire78 <clement.wawszczyk@gmail.com>
2021-02-11 12:38:33 +0100

    fix Uncaught TypeError: Cannot read property 'appendChild' of undefined

--- content.js
+++ content.js
@@ -4,4 +4,7 @@ btn.innerHTML = "View on Github1s";
 btn.classList = "btn ml-2 d-none d-md-block";
 btn.href = window.location.href.replace("https://github.com/", "https://github1s.com/");
 buttonNav = document.getElementsByClassName("file-navigation mb-3 d-flex flex-items-start");
-buttonNav[0].appendChild(btn);
+
+if (buttonNav[0] != null) {
+    buttonNav[0].appendChild(btn);
+}
